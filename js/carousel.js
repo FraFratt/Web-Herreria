0195f959619d71a2f8f8aa742df820bb845b133d
Franco Frattaroli <75225970+FraFratt@users.noreply.github.com>
2022-02-05 14:33:28 -0300

    McCarpinteriaMetalica

---
+++ js/carousel.js
@@ -0,0 +1,18 @@
+// Call carousel manually
+$("#myCarouselCustom").carousel();
+
+// Go to the previous item
+$("#prevBtn").click(function () {
+  $("#myCarouselCustom").carousel("prev");
+});
+// Go to the previous item
+$("#nextBtn").click(function () {
+  $("#myCarouselCustom").carousel("next");
+});
+
+//opciones
+$(".carousel").carousel({
+  interval: 8000,
+  pause: true,
+  wrap: false,
+});
